168f6a2c1da6159ef91ece1b3dc937a848b85341
Jason Axelson <jason.axelson@gmail.com>
2019-04-22 08:32:45 -1000

    Start as its own application

---
+++ lib/data_tracer/application.ex
@@ -0,0 +1,14 @@
+defmodule DataTracer.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {DataTracer, []}
+    ]
+
+    opts = [strategy: :one_for_one, name: DataTracer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule DataTracer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {DataTracer.Application, []},
       extra_applications: [:logger]
     ]
   end
