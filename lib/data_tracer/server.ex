a56d4c92b3ea0561afaac7071617cc3c82aaa49b
Jason Axelson <jason.axelson@gmail.com>
2019-11-03 09:38:11 -1000

    Extract GenServer into a separate module

---
+++ lib/data_tracer/server.ex
@@ -0,0 +1,61 @@
+defmodule DataTracer.Server do
+  use GenServer
+  require Logger
+
+  @table_name :data_tracer
+
+  def start_link(opts, name \\ __MODULE__) do
+    GenServer.start_link(__MODULE__, opts, name: name)
+  end
+
+  def init(_opts) do
+    Logger.debug("DataTracer starting!")
+    table = :ets.new(@table_name, [:set, :protected, :named_table])
+    {:ok, table}
+  end
+
+  def all do
+    :ets.match(@table_name, {:"$1", :"$2"})
+    |> Enum.sort(fn [a, _], [b, _] ->
+      case NaiveDateTime.compare(a, b) do
+        :lt -> false
+        :eq -> true
+        :gt -> true
+      end
+    end)
+  end
+
+  def last do
+    [_time, entry] = all() |> Enum.at(0)
+    entry
+  end
+
+  def store(value, opts \\ []) do
+    key = Keyword.get(opts, :label, NaiveDateTime.utc_now())
+    GenServer.call(__MODULE__, {:store_key, key, value})
+    value
+  end
+
+  def store_key(key, value) do
+    GenServer.call(__MODULE__, {:store_key, key, value})
+    value
+  end
+
+  def lookup(key) do
+    case :ets.lookup(@table_name, key) do
+      [] -> nil
+      [{^key, val}] -> val
+    end
+  end
+
+  def clear do
+    pid = Process.whereis(__MODULE__)
+    Process.exit(pid, :kill)
+  end
+
+  def handle_call({:store_key, key, value}, _from, table) do
+    Logger.warn("Storing #{inspect(key)} => #{inspect(value, pretty: true)}")
+    :ets.insert(table, {key, value})
+    {:reply, :ok, table}
+  end
+end
